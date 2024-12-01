88d927ec272cbcae42681530ce27f20a668e23bb
KotakViraj <virajj@iisc.ac.in>
2024-12-01 17:56:41 +0530

    Add files via upload

---
+++ utilities.py
@@ -0,0 +1,124 @@
+import jax.numpy as np
+import jax
+from jax.nn.initializers import lecun_normal, normal
+from jax.numpy.linalg import eigh, inv, matrix_power
+from jax.scipy.signal import convolve
+
+# Factory for constant initializer in Flax
+def init(x):
+    def _init(key, shape):
+        assert shape == x.shape
+        return x
+
+    return _init
+
+def make_HiPPO(N):
+    P = np.sqrt(1 + 2 * np.arange(N))
+    A = P[:, np.newaxis] * P[np.newaxis, :]
+    A = np.tril(A) - np.diag(np.arange(N))
+    return -A
+
+def make_NPLR_HiPPO(N):
+    nhippo = make_HiPPO(N)
+    P = np.sqrt(np.arange(N) + 0.5)
+    B = np.sqrt(2 * np.arange(N) + 1.0)
+    return nhippo, P, B
+
+# Diagonalize NPLR representation
+def make_DPLR_HiPPO(N):
+    A, P, B = make_NPLR_HiPPO(N)
+    S = A + P[:, np.newaxis] * P[np.newaxis, :]
+
+    # Check skew symmetry
+    S_diag = np.diagonal(S)
+    Lambda_real = np.mean(S_diag) * np.ones_like(S_diag)
+
+    # Diagonalize S to V \Lambda V^*
+    Lambda_imag, V = eigh(S * -1j)
+
+    P = V.conj().T @ P
+    B = V.conj().T @ B
+    return Lambda_real + 1j * Lambda_imag, P, B, V
+
+def hippo_initializer(N):
+    Lambda, P, B, _ = make_DPLR_HiPPO(N)
+    return init(Lambda.real), init(Lambda.imag), init(P), init(B)
+
+def log_step_initializer(dt_min=0.001, dt_max=0.1):
+    def init(key, shape):
+        return jax.random.uniform(key, shape) * (
+            np.log(dt_max) - np.log(dt_min)
+        ) + np.log(dt_min)
+
+    return init
+
+@jax.jit
+def cauchy(v, omega, lambd):
+    """Cauchy matrix multiplication: (n), (l), (n) -> (l)"""
+    cauchy_dot = lambda _omega: (v / (_omega - lambd)).sum()
+    return jax.vmap(cauchy_dot)(omega)
+
+
+def kernel_DPLR(Lambda, P, Q, B, C, step, L):
+    # Evaluate at roots of unity
+    # Generating function is (-)z-transform, so we evaluate at (-)root
+    Omega_L = np.exp((-2j * np.pi) * (np.arange(L) / L))
+
+    aterm = (C.conj(), Q.conj())
+    bterm = (B, P)
+
+    g = (2.0 / step) * ((1.0 - Omega_L) / (1.0 + Omega_L))
+    c = 2.0 / (1.0 + Omega_L)
+
+    # Reduction to core Cauchy kernel
+    k00 = cauchy(aterm[0] * bterm[0], g, Lambda)
+    k01 = cauchy(aterm[0] * bterm[1], g, Lambda)
+    k10 = cauchy(aterm[1] * bterm[0], g, Lambda)
+    k11 = cauchy(aterm[1] * bterm[1], g, Lambda)
+    atRoots = c * (k00 - k01 * (1.0 / (1.0 + k11)) * k10)
+    out = np.fft.ifft(atRoots, L).reshape(L)
+    return out.real
+
+def discrete_DPLR(Lambda, P, Q, B, C, step, L):
+    # Convert parameters to matrices
+    B = B[:, np.newaxis]
+    Ct = C[np.newaxis, :]
+
+    N = Lambda.shape[0]
+    A = np.diag(Lambda) - P[:, np.newaxis] @ Q[:, np.newaxis].conj().T
+    I = np.eye(N)
+
+    # Forward Euler
+    A0 = (2.0 / step) * I + A
+
+    # Backward Euler
+    D = np.diag(1.0 / ((2.0 / step) - Lambda))
+    Qc = Q.conj().T.reshape(1, -1)
+    P2 = P.reshape(-1, 1)
+    A1 = D - (D @ P2 * (1.0 / (1 + (Qc @ D @ P2))) * Qc @ D)
+
+    # A bar and B bar
+    Ab = A1 @ A0
+    Bb = 2 * A1 @ B
+
+    # Recover Cbar from Ct
+    Cb = Ct @ inv(I - matrix_power(Ab, L)).conj()
+    return Ab, Bb, Cb.conj()
+
+def causal_convolution(u, K, nofft=False):
+    if nofft:
+        return convolve(u, K, mode="full")[: u.shape[0]]
+    else:
+        assert K.shape[0] == u.shape[0]
+        ud = np.fft.rfft(np.pad(u, (0, K.shape[0])))
+        Kd = np.fft.rfft(np.pad(K, (0, u.shape[0])))
+        out = ud * Kd
+        return np.fft.irfft(out)[: u.shape[0]]
+
+def scan_SSM(Ab, Bb, Cb, u, x0):
+    def step(x_k_1, u_k):
+        x_k = Ab @ x_k_1 + Bb @ u_k
+        y_k = Cb @ x_k
+        return x_k, y_k
+
+    return jax.lax.scan(step, x0, u)
